21b8270c545274d9fb27613777917677f85ea24f
praveen <praveenleo2311@gmail.com>
2024-04-13 18:29:44 +0530

    feat: header design work done.

--- touristTransportBooking/src/app/app.routes.ts
+++ touristTransportBooking/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
-    {path:'home',component:HomeComponent}
+    {path:'home',component:HomeComponent},
+    {path:'header',component:HeaderComponent}
+
 ];
---
+++ touristTransportBooking/src/app/header/header.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCheckboxModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  schemas: [NO_ERRORS_SCHEMA]
+})
+export class HeaderComponent {
+ headerArea = [
+  {text:'Corporate/Family tours',subText:'Comfort Travel Assured'},
+  {text:'Temple & Family tours',subText:'Organized Easily'},
+  {text:'Educational Tours',subText:'Arranged Instantly'},
+ ]
+
+ 
+}
